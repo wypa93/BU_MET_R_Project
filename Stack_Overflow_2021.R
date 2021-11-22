4e3e62a4e3bc24648352d743d560070673a9ff70
wypa93 <71227540+wypa93@users.noreply.github.com>
2021-11-22 14:04:39 -0500

    test

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -35,13 +35,12 @@ plot_ly(us_data, x = ~Gender)
 us_f_salary<-subset(us_data,Gender = 'Woman')$ConvertedCompYearly
 plot_ly(us_data,x=~CompTotal,color=~Gender,type='histogram')
 
+us_f_salary
 
 
 #search for correlations between salary and age, experience, gender, ethnicity,tech stack, degree
 plot_ly(us_data,x=~YearsCodePro,y=~CompTotal,type = 'scatter',color ='Gender')
 plot_ly(us_data,x=~YearsCodePro,y=~CompTotal,type = 'scatter',color ='Years Code')
-
-
 #identify most popular Tech-Stacks and display them as Word Cloud
 
 
