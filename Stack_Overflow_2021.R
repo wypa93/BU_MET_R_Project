5ab993ccb22c5793a2409045315100ebb4b027ba
wypa93 <71227540+wypa93@users.noreply.github.com>
2021-11-23 11:16:30 -0500

    Gender Pay and Experience Analysis

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -1,6 +1,9 @@
 library(plotly) 
 library(UsingR)
 library(usethis)
+library(dplyr)
+library(tidyr)
+library(sm)
 
 options(scipen=0)
 data<-read.csv('/Users/wypa/Google Drive/Boston University /CS544_Fundamentals_of_R/Project/SO_Survey/survey_results_responses.csv')
@@ -13,31 +16,59 @@ data$YearsCodePro<-as.integer(data$YearsCodePro)
 
 View(us_data)
 
-#Analysis of US Responses only 
-table(data$Gender)
+#Analysis of US Responses only where Total Compensation > US Federal Minimum Wage 2021
 us_data<-subset(data,Country == 'United States of America'&
                 Currency == 'USD\tUnited States dollar'&
-                Gender == 'Man' | Gender == 'Woman')
+                  CompTotal > 15000 &
+                (Gender == 'Man' | Gender == 'Woman'))
 
-plot_ly(us_data,x=~Ethnicity)
 
-#search for and remove outliers using 1.5 IQR
+#search for and remove  outliers in Salary data using 1.5 IQR
 f<-fivenum(us_data$CompTotal);f
 subset(us_data, CompTotal > f[4] + 1.5*(f[4] - f[2])) #investigate upper outliers
-subset(us_data, CompTotal < f[2] - 1.5*(f[4] - f[2])) #investigate lower outliers
 us_data<-subset(us_data,CompTotal<f[4]+1.5*(f[4]-f[2])) #remove upper outliers
-us_data<-subset(us_data,CompTotal>f[2]-1.5*(f[4]-f[2])) #remove lower outliers
 
 plot_ly(us_data, x = ~CompTotal, type="box", name = 'Total Compensation',
         boxpoints = "all", jitter = 0.2, pointpos = -1.5)
 
 
-#visualize distributions age, experience, gender, ethnicity,tech stack, degree
-plot_ly(us_data, x = ~Gender)
-us_f_salary<-subset(us_data,Gender = 'Woman')$ConvertedCompYearly
-plot_ly(us_data,x=~CompTotal,color=~Gender,type='histogram')
+### Analyze Gender Pay Gap
+female <- subset(us_data,Gender == 'Woman')
+male <- subset(us_data,Gender == 'Man')
+density1 <- density(female$CompTotal)
+density2 <- density(male$CompTotal)
 
-us_f_salary
+fig <- plot_ly(x = ~density1$x, y = ~density1$y, type = 'scatter', mode = 'lines', name = 'Women', fill = 'tozeroy')
+fig <- fig %>% add_trace(x = ~density2$x, y = ~density2$y, name = 'Man', fill = 'tozeroy')
+fig <- fig %>% layout(xaxis = list(title = 'Total Compensation'),
+                      yaxis = list(title = 'Density'))
+
+fig
+
+## Analyze Years of Code by Gender
+density3 <- density(drop_na(female,YearsCodePro)$YearsCodePro)
+density4 <- density(drop_na(male,YearsCodePro)$YearsCodePro)
+
+fig <- plot_ly(x = ~density3$x, y = ~density3$y, type = 'scatter', mode = 'lines', name = 'Women', fill = 'tozeroy')
+fig <- fig %>% add_trace(x = ~density4$x, y = ~density4$y, name = 'Man', fill = 'tozeroy')
+fig <- fig %>% layout(xaxis = list(title = 'Total Compensation'),
+                      yaxis = list(title = 'Density'))
+
+fig
+
+
+
+# Analyze Ethnicity Pay Gap
+ethn <- table(us_data$Ethnicity)
+ethn[ethn > 100]
+
+plot_ly(ethn)
+
+white <- density(subset(us_data,Ethnicity == 'White or of European descent')$CompTotal)
+
+
+
+
 
 s#search for correlations between salary and age, experience, gender, ethnicity,tech stack, degree
 plot_ly(us_data,x=~YearsCodePro,y=~CompTotal,type = 'scatter',color ='Gender')
