0a175bf46f6db74d26a764ec0e85ee609da6eba8
wypa93 <71227540+wypa93@users.noreply.github.com>
2021-11-23 13:05:37 -0500

    add Ethnicity Pay Gap

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -48,20 +48,33 @@ fig <- plot_ly(x = ~density3$x, y = ~density3$y, type = 'scatter', mode = 'lines
 fig <- fig %>% add_trace(x = ~density4$x, y = ~density4$y, name = 'Man', fill = 'tozeroy')
 fig <- fig %>% layout(xaxis = list(title = 'Years of Professional Coding Experience'),
                       yaxis = list(title = 'Density'))
-
 fig
 
 
 # Analyze Ethnicity Pay Gap
 ethn <- table(us_data$Ethnicity)
-ethn[ethn > 100]
+ethn<-ethn[ethn > 100]
+names(ethn)
 
-plot_ly(ethn)
-
-white <- density(subset(us_data,Ethnicity == 'White or of European descent')$CompTotal)
-
-
-
+fig <- plot_ly(type = 'scatter', mode = 'line',fill = 'tozeroy')
+fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'Black or of African descent')$CompTotal)$x
+                         , y = ~density(subset(us_data,Ethnicity == 'Black or of African descent')$CompTotal)$y
+                         , name = 'Black or of African descent')
+fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'East Asian')$CompTotal)$x
+                         , y = ~density(subset(us_data,Ethnicity == 'East Asian')$CompTotal)$y
+                         , name = 'East Asian')
+fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'South Asian')$CompTotal)$x
+                         , y = ~density(subset(us_data,Ethnicity == 'South Asian')$CompTotal)$y
+                         , name = 'South Asian')
+fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'Hispanic or Latino/a/x')$CompTotal)$x
+                         , y = ~density(subset(us_data,Ethnicity == 'Hispanic or Latino/a/x')$CompTotal)$y
+                         , name = 'Hispanic or Latino/a/x')
+fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'White or of European descent')$CompTotal)$x
+                         , y = ~density(subset(us_data,Ethnicity == 'White or of European descent')$CompTotal)$y
+                         , name = 'White or of European descent')
+fig <- fig %>% layout(xaxis = list(title = 'Total Compensation'),
+                      yaxis = list(title = 'Density'))
+fig
 
 
 s#search for correlations between salary and age, experience, gender, ethnicity,tech stack, degree
