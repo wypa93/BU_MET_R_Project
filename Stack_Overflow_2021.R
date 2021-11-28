13ca91437caa62228cceefd43117abcef78d8dd2
wypa93 <71227540+wypa93@users.noreply.github.com>
2021-11-28 16:39:35 -0500

    cle

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -5,31 +5,30 @@ library(dplyr)
 library(tidyr)
 library(sampling)
 library(sm)
-
 options(scipen=0)
 data<-read.csv('/Users/wypa/Google Drive/Boston University /CS544_Fundamentals_of_R/Project/SO_Survey/survey_results_responses.csv')
 View(data)
 
-
-### investigate and transform datatypes where required ###
+### PRE-PROCESSING ###
+# investigate and transform datatypes where required ###
 colnames(us_data)
 data$TotalComp<-as.intger(data$TotalComp)
 data$YearsCode<-as.integer(data$YearsCode)
 data$YearsCodePro<-as.integer(data$YearsCodePro)
-
 #Analysis of US Responses only where Total Compensation > US Federal Minimum Wage 2021
 us_data<-subset(data,Country == 'United States of America'&
                 Currency == 'USD\tUnited States dollar'&
                   CompTotal > 15000 &
                 (Gender == 'Man' | Gender == 'Woman'))
-
-
 #search for and remove  outliers in Salary data using 1.5 IQR
 f<-fivenum(us_data$CompTotal);f
 subset(us_data, CompTotal > f[4] + 1.5*(f[4] - f[2])) 
 us_data<-subset(us_data,CompTotal<f[4]+1.5*(f[4]-f[2])) 
 
-### Analyze Pay Gap by Gender
+
+
+
+### ANALYSIS OF GENDER DIFFERENCES ###
 female <- subset(us_data,Gender == 'Woman')
 male <- subset(us_data,Gender == 'Man')
 density1 <- density(female$CompTotal)
@@ -38,9 +37,7 @@ density2 <- density(male$CompTotal)
 fig <- plot_ly(x = ~density1$x, y = ~density1$y, type = 'scatter', mode = 'lines', name = 'Women', fill = 'tozeroy')
 fig <- fig %>% add_trace(x = ~density2$x, y = ~density2$y, name = 'Man', fill = 'tozeroy')
 fig <- fig %>% layout(xaxis = list(title = 'Total Compensation'),
-                      yaxis = list(title = 'Density'))
-fig
-
+                      yaxis = list(title = 'Density'));fig
 ## Analyze Years of Professional Code Experience by Gender
 density3 <- density(drop_na(female,YearsCodePro)$YearsCodePro)
 density4 <- density(drop_na(male,YearsCodePro)$YearsCodePro)
@@ -48,9 +45,7 @@ density4 <- density(drop_na(male,YearsCodePro)$YearsCodePro)
 fig <- plot_ly(x = ~density3$x, y = ~density3$y, type = 'scatter', mode = 'lines', name = 'Women', fill = 'tozeroy')
 fig <- fig %>% add_trace(x = ~density4$x, y = ~density4$y, name = 'Man', fill = 'tozeroy')
 fig <- fig %>% layout(xaxis = list(title = 'Years of Professional Coding Experience'),
-                      yaxis = list(title = 'Density'))
-fig
-
+                      yaxis = list(title = 'Density'));fig
 ## Analyze Education Level by Gender
 ed_tab_m <- table(male$EdLevel)
 ed_tab_m <- ed_tab_m / sum(as.numeric(ed_tab_m))
@@ -64,7 +59,7 @@ fig <- plot_ly(type='bar',y=round(as.numeric(ed_tab_m),2),x=names(ed_tab_m),name
 fig %>% add_trace(type='bar',y=round(as.numeric(ed_tab_f),2),x=names(ed_tab_f),name='Woman')
 
 
-# Analyze Pay Gap by Ethnicity
+### Analyze Pay Gap by Ethnicity ###
 ethn <- table(us_data$Ethnicity)
 ethn<-ethn[ethn > 100]
 names(ethn)
@@ -88,11 +83,6 @@ fig <- fig %>% add_trace(x = ~density(subset(us_data,Ethnicity == 'White or of E
 fig <- fig %>% layout(xaxis = list(title = 'Total Compensation'),
                       yaxis = list(title = 'Density'))
 fig
-
-
-#search for correlations between salary and age, experience, gender, ethnicity,tech stack, degree
-plot_ly(us_data,x=~YearsCodePro,y=~CompTotal,type = 'scatter',color ='Gender')
-plot_ly(us_data,x=~YearsCodePro,y=~CompTotal,type = 'scatter',color ='Years Code')
 
 
 ## Central Limit Theorem ##
@@ -123,6 +113,7 @@ for (i in sample.sizes){
   sample.dev <- c(sample.dev,sd(x))
 }
 sprintf('Sample Size: %i, Mean: %f, Standard Deviation, %f',sample.sizes,sample.means,sample.dev)
+
 
 
 #identify most popular Tech-Stacks and display them as Word Cloud
