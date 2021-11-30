91335ac38f593852e0a75fab83f6c07c13a22550
wypa93 <71227540+wypa93@users.noreply.github.com>
2021-11-30 11:56:09 -0500

    updated

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -16,20 +16,24 @@ options(scipen=0)
 data<-read.csv('/Users/wypa/Google Drive/Boston University /CS544_Fundamentals_of_R/Project/SO_Survey/survey_results_responses.csv')
 # Open from Ray's PC
 data<-read.csv('/Users/rayhan/Documents/School/BU/1) Fall 2021/CS 544 (Foundations of Analytics with R)/Final Project/BU_MET_R_Project/survey_results_responses.csv')
-View(data)
 
 
 
 ### PRE-PROCESSING ###
-# investigate and transform datatypes where required ###
-data$TotalComp<-as.integer(data$TotalComp) #typo here previously as.intger (Ray)
-data$YearsCode<-as.integer(data$YearsCode)
-data$YearsCodePro<-as.integer(data$YearsCodePro)
+#Remove Rows where Numeric Values are missing
+data <- subset(data,is.na(data$CompTotal)==FALSE &
+                 is.na(data$YearsCode)==FALSE &
+                 (is.na(data$YearsCodePro)==FALSE))
 #Analysis of US Responses only where Total Compensation > US Federal Minimum Wage 2021
 us_data<-subset(data,Country == 'United States of America'&
-                Currency == 'USD\tUnited States dollar'&
+                  Currency == 'USD\tUnited States dollar'&
                   CompTotal > 15000 &
-                (Gender == 'Man' | Gender == 'Woman'))
+                  (Gender == 'Man' | Gender == 'Woman'))
+# investigate and transform datatypes where required ###
+us_data$CompTotal<-as.integer(us_data$CompTotal)
+us_data$YearsCode<-as.integer(us_data$YearsCode)
+us_data$YearsCodePro<-as.integer(us_data$YearsCodePro)
+
 #search for and remove  outliers in Salary data using 1.5 IQR
 f<-fivenum(us_data$CompTotal);f
 subset(us_data, CompTotal > f[4] + 1.5*(f[4] - f[2])) 
