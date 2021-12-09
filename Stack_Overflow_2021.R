bfb85672120c8b1081367378b0df5ddc2c1d0ada
Maksim Romanchuk <maksimromancuk@maksimromanchuk.local>
2021-12-08 21:02:33 -0500

    hope it's final

--- Stack_Overflow_2021.R
+++ Stack_Overflow_2021.R
@@ -251,40 +251,50 @@ hist(data_1_no_out$ConvertedCompYearly, breaks = c(seq(from = min_val, to = max_
 fivenum(data_1_no_out$ConvertedCompYearly)
 boxplot()
 
+
+#MAXIM SAMPLING HOPE FINAL
 library(plotly)
-fig1<-plot_ly(data_1_no_out,x=~ConvertedCompYearly, type = "histogram",name='The population dataset')%>%
+#data cleaning
+us_data_1 <- us_data[us_data$ConvertedCompYearly %in% na.omit(us_data$ConvertedCompYearly) , ]
+ten_perc <- nrow(us_data_1)*0.1
+min_out <- fivenum(us_data_1$ConvertedCompYearly)[2] - 1.5*(fivenum(us_data_1$ConvertedCompYearly)[4]-fivenum(us_data_1$ConvertedCompYearly)[2])
+max_out <- fivenum(us_data_1$ConvertedCompYearly)[4] + 1.5*(fivenum(us_data_1$ConvertedCompYearly)[4]-fivenum(us_data_1$ConvertedCompYearly)[2])
+us_data_1 <- us_data_1[(us_data_1$ConvertedCompYearly> min_out) & (us_data_1$ConvertedCompYearly< max_out), ]
+ten_perc <- nrow(us_data_1)*0.1
+
+#plots of population
+fig1<-plot_ly(us_data_1,x=~ConvertedCompYearly, type = "histogram",name='The population dataset', nbinsx = 20)%>%
   layout(xaxis= list(showticklabels = FALSE)); fig1
 
-fig1_bx <- plot_ly(data_1_no_out,x=~ConvertedCompYearly, type = "box",name='The population dataset')%>%
+fig1_bx <- plot_ly(us_data_1,x=~ConvertedCompYearly, type = "box",name='The population dataset')%>%
   layout(xaxis= list(showticklabels = FALSE)); fig1_bx
 
 
-colnames(data_1_no_out)
 #sampling -- srs without replacement
-#take the most popular countries
-top_states <- sort(table(data_1_no_out$US_State),decreasing=TRUE)[1:5]
-#taking the subset of these countries
-states_srs <- subset(data_1_no_out,data_1_no_out$US_State %in% names(top_states))
+#take the most popular states
+top_states <- sort(table(us_data_1$US_State),decreasing=TRUE)[1:5]
+#taking the subset of these states
+states_srs <- subset(us_data_1,us_data_1$US_State %in% names(top_states))
 set.seed(9999)
-size = 700
+size = ten_perc
 #randomly chooses rows for further analysis
 s<-srswor(size, nrow(states_srs))
-states_srs_500 <- countries_srs[s != 0, ]
-fig2<-plot_ly(states_srs_500,x=~ConvertedCompYearly, type = "histogram",name='SRS without replacement')%>%
+states_srs <- states_srs[s != 0, ]
+fig2<-plot_ly(states_srs,x=~ConvertedCompYearly, type = "histogram",name='SRS without replacement', nbinsx = 20)%>%
   layout(xaxis= list(showticklabels = FALSE)); fig2
-fig2_bx <- plot_ly(states_srs_500,x=~ConvertedCompYearly, type = "box",name='SRS without replacement')%>%
+fig2_bx <- plot_ly(states_srs,x=~ConvertedCompYearly, type = "box",name='SRS without replacement')%>%
   layout(xaxis= list(showticklabels = FALSE)); fig2_bx
 
 
-#making the subset of interested countries for further convenience
-subset_states <- subset(data_1_no_out,data_1_no_out$US_State %in% names(top_states))
+#making the subset of interested states for further convenience
+subset_states <- subset(us_data_1,us_data_1$US_State %in% names(top_states))
 
 # -- systematic sampling
-k <- ceiling(nrow(subset_states)/size) #N rows are divided into n(50) groups and each group has k items
+k <- ceiling(nrow(subset_states)/size) #N rows are divided into n = size groups and each group has k items
 r<-sample(k, 1)#random item from k is selected
 indexes = seq(r, by = k, length = size) #all items are selected by taking every k-th item from the frame
 subset_systematic <- subset_states[indexes, ]
-fig3<-plot_ly(subset_systematic,x=~ConvertedCompYearly, type = "histogram",name='Systematic Sampling')%>%
+fig3<-plot_ly(subset_systematic,x=~ConvertedCompYearly, type = "histogram",name='Systematic Sampling', nbinsx = 20)%>%
   layout(xaxis= list(showticklabels = FALSE)); fig3
 fig3_bx <- plot_ly(subset_systematic,x=~ConvertedCompYearly, type = "box",name='Systematic Sampling')%>%
   layout(xaxis= list(showticklabels = FALSE)); fig3_bx
@@ -295,8 +305,7 @@ pik<-inclusionprobabilities(subset_states$ConvertedCompYearly,size)
 sum(pik)
 s<-UPsystematic(pik)
 sample<-subset_states[s!=0,]
-fig4<-plot_ly(sample, x=~ConvertedCompYearly, type = "histogram",name='Inclusion probabilities')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig4
+fig4<-plot_ly(sample, x=~ConvertedCompYearly, type = "histogram",name='Inclusion probabilities', nbinsx = 20); fig4
 fig4_bx <- plot_ly(sample,x=~ConvertedCompYearly, type = "box",name='Inclusion probabilities')%>%
   layout(xaxis= list(showticklabels = FALSE)); fig4_bx
 
@@ -309,7 +318,7 @@ st.1 <- sampling::strata(subset_states, stratanames = c("US_State"),
                          size = size_st, method = "srswor",
                          description = TRUE)
 st.sample1 <- getdata(subset_states, st.1)
-fig5<-plot_ly(st.sample1, x=~ConvertedCompYearly, type = "histogram",name='Stratified Sampling')%>%
+fig5<-plot_ly(st.sample1, x=~ConvertedCompYearly, type = "histogram",name='Stratified Sampling', nbinsx = 20)%>%
   layout(xaxis= list(showticklabels = FALSE)); fig5
 fig5_bx <- plot_ly(st.sample1,x=~ConvertedCompYearly, type = "box",name='Stratified Sampling')%>%
   layout(xaxis= list(showticklabels = FALSE)); fig5_bx
@@ -321,68 +330,4 @@ fig_bx <- plotly:: subplot(fig1_bx,fig2_bx,fig3_bx,fig4_bx, fig5_bx, nrows =5)%>
   layout(showlegend = FALSE)
 fig_bx
 
-#So,it's seen that sampling methods poorly represnt the data as the distributions from samping method and population differ a lot.
 
-
-#SAMPLING BASED ON COUNTRY
-colnames(data_1_no_out)
-#sampling -- srs without replacement
-#take the most popular countries
-top_countries <- sort(table(data_1_no_out$Country),decreasing=TRUE)[1:5]
-#taking the subset of these countries
-countries_srs <- subset(data_1_no_out,data_1_no_out$Country %in% names(top_countries))
-set.seed(9999)
-size = 700
-#randomly chooses rows for further analysis
-s<-srswor(size, nrow(countries_srs))
-countries_srs_500 <- countries_srs[s != 0, ]
-fig2<-plot_ly(countries_srs_500,x=~ConvertedCompYearly, type = "histogram",name='SRS without replacement')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig2
-fig2_bx <- plot_ly(countries_srs_500,x=~ConvertedCompYearly, type = "box",name='SRS without replacement')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig2_bx
-
-
-#making the subset of interested countries for further convenience
-subset_countries <- subset(data_1_no_out,data_1_no_out$Country %in% names(top_countries))
-
-# -- systematic sampling
-k <- ceiling(nrow(subset_countries)/size) #N rows are divided into n(50) groups and each group has k items
-r<-sample(k, 1)#random item from k is selected
-indexes = seq(r, by = k, length = size) #all items are selected by taking every k-th item from the frame
-subset_systematic <- subset_countries[indexes, ]
-fig3<-plot_ly(subset_systematic,x=~ConvertedCompYearly, type = "histogram",name='Systematic Sampling')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig3
-fig3_bx <- plot_ly(subset_systematic,x=~ConvertedCompYearly, type = "box",name='Systematic Sampling')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig3_bx
-
-
-#-- inclusion probabilities
-pik<-inclusionprobabilities(subset_countries$ConvertedCompYearly,size)
-sum(pik)
-s<-UPsystematic(pik)
-sample<-subset_countries[s!=0,]
-fig4<-plot_ly(sample, x=~ConvertedCompYearly, type = "histogram",name='Inclusion probabilities')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig4
-fig4_bx <- plot_ly(sample,x=~ConvertedCompYearly, type = "box",name='Inclusion probabilities')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig4_bx
-
-
-#--stratified sampling based on the Country variable
-subset_countries<-subset_countries[order(subset_countries$Country),]
-size_st<-table(subset_countries$Country)/sum(table(subset_countries$Country))*size
-
-st.1 <- sampling::strata(subset_countries, stratanames = c("Country"),
-                         size = size_st, method = "srswor",
-                         description = TRUE)
-st.sample1 <- getdata(subset_states, st.1)
-fig5<-plot_ly(st.sample1, x=~ConvertedCompYearly, type = "histogram",name='Stratified Sampling')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig5
-fig5_bx <- plot_ly(st.sample1,x=~ConvertedCompYearly, type = "box",name='Stratified Sampling')%>%
-  layout(xaxis= list(showticklabels = FALSE)); fig5_bx
-fig <- plotly:: subplot(fig1,fig2,fig3,fig4, fig5, nrows =5)%>%
-  layout(showlegend = FALSE)
-fig
-
-fig_bx_countries <- plotly:: subplot(fig1_bx,fig2_bx,fig3_bx,fig4_bx, fig5_bx, nrows =5)%>%
-  layout(showlegend = FALSE)
-fig_bx_countries
